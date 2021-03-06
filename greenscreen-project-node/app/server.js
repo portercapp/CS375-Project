0d9433862b4bba459263f07da5b2a1acdfe87724
Darren Shaw <darren2110@gmail.com>
2021-03-05 19:22:06 -0500

    Added background image upload handler

--- greenscreen-project-node/app/server.js
+++ greenscreen-project-node/app/server.js
@@ -51,6 +51,33 @@ app.post('/upload-profile-pic', (req, res) => {
     });
 });
 
+app.post('/upload-background-pic', (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('background_pic');
+
+    upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+       
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="${req.file.filename}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
+});
+
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);
 });
