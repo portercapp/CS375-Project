df0781be6786bcbb00a2c2981b5cff2bbc21f335
Darren Shaw <darren2110@gmail.com>
2021-03-11 16:03:37 -0500

    Renamed sloth images to simple bg 1, 2, and 3 to match values passed by radio selector. Displayed uploaded/selected images when uploaded.

--- greenscreen-project-node/app/server.js
+++ greenscreen-project-node/app/server.js
@@ -26,6 +26,7 @@ const storage = multer.diskStorage({
 
 app.post('/upload-pictures', (req, res) => {
     // 'profile_pic' is the name of our file input field in the HTML form
+    // 'background_pic' is the name of our file input field in the HTML form
     let upload = multer({ storage: storage, fileFilter: helpers.imageFilter })
     .fields([
             { name: 'profile_pic', maxCount: 1},
@@ -46,18 +47,24 @@ app.post('/upload-pictures', (req, res) => {
         else if(!req.files['background_pic']){
             //No uploaded background - CHECK if a radio button was ticked
             if (req.body['bg-radio'] !== undefined) {
-                res.send(`Selected ${req.body['bg-radio']}`);
+                res.send(
+                    `You have uploaded this profile image: <hr/><img src="${req.files[`profile_pic`][0].filename}" width="500"><hr /><a href="./">Upload another image</a>
+                    <br/>You have selected this background image: <hr/><img src="${req.body['bg-radio']}.jpg" width="500"><hr /><a href="./">Upload another image</a>`
+                    );
             }else{
                 res.send("Upload a background or select a template background.")
             }
         }
         else if (err instanceof multer.MulterError || err) {
             return res.send(err);
+        }else{
+            //Both images were uploaded successfully
+            //Display uploaded image for user validation
+            res.send(
+                `You have uploaded this profile image: <hr/><img src="${req.files[`profile_pic`][0].filename}" width="500"><hr /><a href="./">Upload another image</a>
+                 <br/>You have uploaded this background image: <hr/><img src="${req.files[`background_pic`][0].filename}" width="500"><hr /><a href="./">Upload another image</a>`
+            ); //The [0] is required to access the file attributes such as name, path, etc.
         }
-
-       
-        // Display uploaded image for user validation
-        //res.send(`You have uploaded this image: <hr/><img src="${req.file.filename}" width="500"><hr /><a href="./">Upload another image</a>`);
     });
 });
 
